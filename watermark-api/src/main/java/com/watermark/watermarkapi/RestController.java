e48bf5819ccbca2da9f3a217850a57082cad140b
ELiza <235543@student.pwr.edu.pl>
2020-11-09 17:36:05 +0100

    Adding restController

---
+++ watermark-api/src/main/java/com/watermark/watermarkapi/RestController.java
@@ -0,0 +1,20 @@
+package com.watermark.watermarkapi;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class RestController {
+
+    @RequestMapping(value={"/algorithm"}, method= RequestMethod.POST,  produces = {"application/json"})
+    public @ResponseBody
+    String getAttr(@RequestParam String url, @RequestParam String algorithmType)
+    {
+        return "Url: " + url + "Algorithm: " + algorithmType;
+    }
+
+
+}
